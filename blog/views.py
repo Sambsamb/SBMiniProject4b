d0ab3f1e0b130d347ae6bb19e566f7b9d74295a0
Samb <samb@townsware.com>
2022-11-05 10:53:28 -0400

    SB - home

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+"""
+INF601 - Advanced Programming in Python
+Mini Project 4 - Django small web app
+Sam Boutros
+Prof. Zeller
+FHSU - Fall 2022
+11/6/2022
+"""
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'blog/home.html')
