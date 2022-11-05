7035b48f5ffdd9d5b1c9e9b438620e08afd0537c
Samb <samb@townsware.com>
2022-11-05 10:08:34 -0400

    SB - Extend Anstractuser

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,29 @@
+"""
+INF601 - Advanced Programming in Python
+Mini Project 4 - Django small web app
+Sam Boutros
+Prof. Zeller
+FHSU - Fall 2022
+11/6/2022
+"""
+
+# Extending  AbstractUser which includes all the functionality of the default  User  class.
+# Adding two additional fields:
+# ImageField  containing a profile photo, and
+# role, which will differentiate between two types of users; creators and subscribers
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Creator'),
+        (SUBSCRIBER, 'Subscriber'),
+    )
+    profile_photo = models.ImageField()
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
+
