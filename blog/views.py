f4a6651a9ca1cbe9f072e47afa4e0898bc7396a1
Samb <samb@townsware.com>
2022-11-05 10:59:01 -0400

    SB - login required to access home

--- blog/views.py
+++ blog/views.py
@@ -7,8 +7,10 @@ FHSU - Fall 2022
 11/6/2022
 """
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def home(request):
     return render(request, 'blog/home.html')
