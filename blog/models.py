e4ce8298c827a7d2f698b349a375855f01ddbe47
Samb <samb@townsware.com>
2022-11-05 11:37:50 -0400

    SB - Blog and Photo models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
+"""
+INF601 - Advanced Programming in Python
+Mini Project 4 - Django small web app
+Sam Boutros
+Prof. Zeller
+FHSU - Fall 2022
+11/6/2022
+"""
+# blog/models.py
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    image = models.ImageField()
+    caption = models.CharField(max_length=128, blank=True)
+    uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Blog(models.Model):
+    photo = models.ForeignKey(Photo, null=True, on_delete=models.SET_NULL, blank=True)
+    # blog posts can be optionally linked to a  photo  by the  ForeignKey  relationship
+    title = models.CharField(max_length=128)
+    content = models.CharField(max_length=5000)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    starred = models.BooleanField(default=False)
